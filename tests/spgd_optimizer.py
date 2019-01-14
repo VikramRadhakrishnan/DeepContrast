5cc8bd8c0c3319a00f3806041b2d88bb340bc8a3
Vikram Radhakrishnan <radhakrishnan@mail.strw.leidenuniv.nl>
2019-01-14 16:37:32 +0100

    Add atmosphere tracking to the code

--- tests/spgd_optimizer.py
+++ tests/spgd_optimizer.py
@@ -35,9 +35,17 @@ wfs_focal_grid = make_focal_grid(pupil_grid, 8, 20, wavelength)
 aperture = circular_aperture(D)
 
 # Telescope parameters
-Dtel=1
+Dtel=4
 tel_pupil_grid = make_pupil_grid(N, Dtel)
 tel_aperture = circular_aperture(Dtel)
+
+## Create the deformable mirror
+actuator_grid = make_pupil_grid(25, D*1.1)
+sigma = D/25
+gaussian_basis = make_gaussian_pokes(pupil_grid, actuator_grid, sigma)
+dm = DeformableMirror(gaussian_basis)
+num_modes = len(dm.influence_functions)
+dm.actuators = np.zeros(num_modes)
 
 # Atmosphere parameters
 pixels_per_frame = 1
@@ -48,27 +56,26 @@ height = 0
 
 # Make atmosphere
 np.random.seed(42)
-layers = [InfiniteAtmosphericLayer(tel_pupil_grid, Cn_squared_from_fried_parameter(r0, 500e-9), L0, velocity * tel_pupil_grid.delta[0], height, 2)]
+#layers = [InfiniteAtmosphericLayer(tel_pupil_grid, Cn_squared_from_fried_parameter(r0, 500e-9), L0, velocity * tel_pupil_grid.delta[0], height, 2)]
+layers = []
+layer = InfiniteAtmosphericLayer(tel_pupil_grid, Cn_squared_from_fried_parameter(r0, 500e-9), L0, velocity * tel_pupil_grid.delta[0], height, 2)
+layer2 = ModalAdaptiveOpticsLayer(layer, dm.influence_functions, 1)
+layers.append(layer2)
 atmosphere = MultiLayerAtmosphere(layers, False)
 
+# Make initial phasescreen
 wf_tel = Wavefront(tel_aperture(tel_pupil_grid), wavelength)
+atms_time = 1
+atmosphere.evolve_until(atms_time)
 wfatms_tel = atmosphere.forward(wf_tel)
 
 ## Demagnify wavefront and phase-screen for the optics
-mag = Magnifier(10.5e-3/1)
+mag = Magnifier(10.5e-3/4)
 wf = mag.forward(wf_tel)
 wfatms = mag.forward(wfatms_tel)
 
 ## Create propagator from pupil to focal plane
 prop = FraunhoferPropagator(pupil_grid, science_focal_grid, wavelength)
-
-## Create the deformable mirror
-actuator_grid = make_pupil_grid(22, D*1.1)
-sigma = D/22.
-gaussian_basis = make_gaussian_pokes(pupil_grid, actuator_grid, sigma)
-dm = DeformableMirror(gaussian_basis)
-num_modes = len(dm.influence_functions)
-dm.actuators = np.zeros(num_modes)
 
 ## Get the app coronagraph
 app_amp = fits.getdata('/home/vikram/Work/DeepContrast/coronagraphs/Square_20_80_20_25_0_2_amp_resampled_512.fits').ravel()
@@ -87,7 +94,7 @@ science_camera = NoiselessDetector()
 shack_hartmann_camera = NoiselessDetector()
 
 ## Create a spatial filter
-filt_aperture = circular_aperture(22)
+filt_aperture = circular_aperture(25)
 spatial_filter = Apodizer(filt_aperture(science_focal_grid))
 
 ## Create the Shack-Hartmann Wavefront sensor
@@ -118,13 +125,10 @@ num_coeffs = 40
 plaw_index = -1.
 np.random.seed(42)
 coeffs = ((np.random.rand(num_coeffs) - 0.5) * 2 ) * (np.arange(num_coeffs) + 1) ** plaw_index
+coeffs = np.zeros(coeffs.shape)
 zernike_basis = make_zernike_basis(num_coeffs, D, pupil_grid, 2)
 ncp_phase = np.dot(zernike_basis.transformation_matrix, coeffs)
 ncp = Apodizer(np.exp(1j * ncp_phase))
-
-## Make intial phasescreen
-atmosphere.evolve_until(1)
-wfatms = atmosphere.forward(wf)
 
 ########### The test starts here #############################################
 
@@ -133,14 +137,15 @@ cost_function = []
 strehl_evol = []
 contrast_evol = []
 modes_evolution = []
+lrate_evol = []
 
 # Reset the learning rate, gradients, and perturbatory sequence
-lrate = 0.001
-amp = 1e-9
-iterations = 100000
+lrate = 1e-6
+amp = 1e-10
+iterations = 10000
 merit = 999.99e9
 grads = np.zeros(dm.actuators.size,)
-dm_sequence = np.random.choice([-1, 1], size=(num_modes, iterations)) * amp
+np.random.seed(42)
 
 # Start inner loop closed-loop control here
 for loop in np.arange(iterations):
@@ -160,15 +165,12 @@ for loop in np.arange(iterations):
     wfout = app.forward(reconstruct_wf)
     test_img = prop(wfout).power
     
-    actual_strehl = sci_img[np.argmax(diff_lim_img)] / diff_lim_img.max()
-    actual_contrast = sci_img[dz_ind].mean() / diff_lim_img.max()
-    
     test_strehl = test_img[np.argmax(diff_lim_img)] / diff_lim_img.max()
     test_contrast = test_img[dz_ind].mean() / diff_lim_img.max()
     new_merit = np.sqrt(test_contrast) / test_strehl
     
     # If the update in the cost function is small, or the merit has worsened
-    if merit - new_merit < 1e-4:
+    if merit - new_merit < 1e-8:
         # If there has been an improvement (decrease) in the cost function
         # Then increase the learning rate
         if new_merit < merit:
@@ -182,18 +184,18 @@ for loop in np.arange(iterations):
     merit = new_merit
     cost_function.append(merit)
     
-    strehl_evol.append(actual_strehl)
-    contrast_evol.append(actual_contrast)
+    strehl_evol.append(test_strehl)
+    contrast_evol.append(test_contrast)
     modes_evolution.append(dm.actuators.copy())
+    lrate_evol.append(lrate)
     
     if loop % 1000 == 0:
-        print("Loop {0}: Calculated strehl: {1:.4f} contrast: {2:.4E} cost: {3:.4E}\
-              \n Actual strehl: {4:.4f} contrast: {5:.4E}\n"\
-              .format(loop, test_strehl, test_contrast, merit,\
-                      actual_strehl, actual_contrast))
+        print("Loop {0}: Calculated strehl: {1:.4f} contrast: {2:.4E} cost: {3:.4E}\n"\
+              .format(loop, test_strehl, test_contrast, merit))
     
     # Make a measurement of the metric with DM actuators with delta added
-    dm.actuators += dm_sequence[:,loop]
+    dm_sequence = np.random.choice([-1,1], dm.actuators.shape) * amp
+    dm.actuators += dm_sequence
     dm_wf = dm.forward(wfatms)
     reconstruct_wf = prop.backward(spatial_filter.forward(prop(dm_wf)))
     wfout = app.forward(reconstruct_wf)
@@ -208,7 +210,101 @@ for loop in np.arange(iterations):
     LF = merit_delta - merit
     
     # Multiply by the delta amplitudes vector and divide by amp^2 to get gradient
-    grads = (LF * dm_sequence[:,loop])# / (amp ** 2)
+    grads = (LF * dm_sequence)# / (amp ** 2)
     
     # Calculate new DM actuator positions using learning rate and gradient
     dm.actuators -= lrate * grads
+
+# Save dm actuators after loop 1
+dm_loop1 = dm.actuators.copy()
+test_strehl_loop1 = test_strehl
+test_contrast_loop1 = test_contrast
+postloop_lrate = lrate
+
+wf_loop1 = dm_wf.copy()
+sci_img1 = sci_img.copy()
+
+phasescreens = []
+phasescreens.append(wfatms.copy())
+# Now run the loop while tracking atmospheric changes
+for opt_loop in np.arange(4000):
+    # Update the atmospheric turbulence
+    if opt_loop % 1000 == 0:
+        print("Phasescreen changes\n")
+        atms_time += 1
+        atmosphere.evolve_until(atms_time)
+        wfatms_tel = atmosphere.forward(wf_tel)
+        wfatms = mag.forward(wfatms_tel)
+        phasescreens.append(wfatms)
+        lrate = 1e-6
+        merit = 999.99e9
+        #dm.actuators = np.zeros(dm.actuators.shape)
+    
+    # Common path here
+    dm_wf = dm.forward(wfatms)
+    
+    # Science optical path here
+    ncp_wf = ncp.forward(dm_wf)
+    app_wf = app.forward(ncp_wf)
+    science_camera.integrate(prop(app_wf), dt=1e-3)
+    
+    # Wavefront sensor optical path here
+    # Implement spatial filter to simulate reconstructed wavefront
+    reconstruct_wf = prop.backward(spatial_filter.forward(prop(dm_wf)))
+
+    # Control path here
+    wfout = app.forward(reconstruct_wf)
+    test_img = prop(wfout).power
+    
+    test_strehl = test_img[np.argmax(diff_lim_img)] / diff_lim_img.max()
+    test_contrast = test_img[dz_ind].mean() / diff_lim_img.max()
+    new_merit = np.sqrt(test_contrast) / test_strehl
+    
+    # If the update in the cost function is small
+    if merit - new_merit < 1e-8:
+        # If there has been an improvement (decrease) in the cost function
+        # Then increase the learning rate
+        if new_merit < merit:
+            lrate *= 1.05
+        # Otherwise decrease learning rate and go back to previous actuator values
+        else:
+            dm.actuators = modes_evolution[-1].copy()
+            new_merit = merit
+            lrate *= 0.7
+            
+    merit = new_merit
+    
+    cost_function.append(merit)
+    strehl_evol.append(test_strehl)
+    contrast_evol.append(test_contrast)
+    lrate_evol.append(lrate)
+    
+    modes_evolution.append(dm.actuators.copy())
+    
+    if opt_loop % 100 == 0:
+        print("Tracking loop {0}: Calculated strehl: {1:.4f} contrast: {2:.4E} cost: {3:.4E}\n"\
+              .format(opt_loop, test_strehl, test_contrast, merit))
+                
+    # Make a measurement of the metric with DM actuators with delta added
+    dm_sequence = np.random.choice([-1,1], dm.actuators.shape) * amp
+    dm.actuators += dm_sequence 
+    dm_wf = dm.forward(wfatms)
+    reconstruct_wf = prop.backward(spatial_filter.forward(prop(dm_wf)))
+    wfout = app.forward(reconstruct_wf)
+    test_img = prop(wfout).power
+    
+    test_strehl = test_img[np.argmax(diff_lim_img)] / diff_lim_img.max()
+    test_contrast = test_img[dz_ind].mean() / diff_lim_img.max()
+    merit_delta = np.sqrt(test_contrast) / test_strehl
+    
+    # Take the difference between measurements
+    # This is equivalent to having a low pass filter of the cost function
+    LF = merit_delta - merit
+    
+    # Multiply by the delta amplitudes vector and divide by amp^2 to get gradient
+    grads = (LF * dm_sequence) #/ (amp ** 2)
+    
+    # Calculate new DM actuator positions using learning rate and gradient
+    dm.actuators -= lrate * grads
+
+final_sci_img = science_camera.read_out()
